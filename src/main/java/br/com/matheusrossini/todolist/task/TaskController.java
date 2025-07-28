24dd12896b74c858af942354f257da254f2be17a
matheus rossini <matheus.rossini01@gmail.com>
2025-07-28 12:50:15 -0300

    Atualização da API

--- src/main/java/br/com/matheusrossini/todolist/task/TaskController.java
+++ src/main/java/br/com/matheusrossini/todolist/task/TaskController.java
@@ -6,8 +6,10 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,63 +18,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.matheusrossini.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    
+
     @Autowired
     private ITaskRepository taskRepository;
 
-
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request ){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
 
         var currentDate = LocalDateTime.now();
 
-        if (currentDate.isAfter(taskModel.getStartAt())  || currentDate.isAfter(taskModel.getEndAt())) {
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body("A data de início e de témino deve ser maior que a data atual");
+                    .body("A data de início e de témino deve ser maior que a data atual");
         }
 
         if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body("A data de início e deve ser menor que a data de témino");
+                    .body("A data de início e deve ser menor que a data de témino");
         }
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
-    
+
     @GetMapping("/")
-    
-    public List<TaskModel> list(HttpServletRequest request){
+
+    public List<TaskModel> list(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
-        var tasks =  this.taskRepository.findByIdUser((UUID) idUser);
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id ){
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
 
-
-         if (task == null) {
+        if (task == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("Tarefa não encontrada");
-         }
+                    .body("Tarefa não encontrada");
+        }
 
         var idUser = request.getAttribute("idUser");
 
-        if(!task.getIdUser().equals(idUser)) {
+        if (!task.getIdUser().equals(idUser)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("Usuario nao tem permissao para alterar essa tarefa");
+                    .body("Usuario nao tem permissao para alterar essa tarefa");
         }
 
         Utils.copyNonNullProperties(taskModel, task);
@@ -81,6 +77,16 @@ public class TaskController {
         return ResponseEntity.ok().body(taskUpdate);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable UUID id) {
+        var userOptional = this.taskRepository.findById(id);
+
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task Usuário");
+        }
+
+        this.taskRepository.deleteById(id);
+        return ResponseEntity.ok("Task deletada com sucesso !!!");
+
+    }
 }
-
-
--- src/main/java/br/com/matheusrossini/todolist/user/UserController.java
+++ src/main/java/br/com/matheusrossini/todolist/user/UserController.java
@@ -1,9 +1,16 @@
 package br.com.matheusrossini.todolist.user;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +24,11 @@ public class UserController {
     @Autowired
     private IuserRepository userRepository;
 
-
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
-
-        if(user != null){
+        if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario Já existe");
         }
 
@@ -36,6 +41,51 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userCreated);
     }
 
-    
-    
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable UUID id, @RequestBody UserModel userModel) {
+        var userOptional = this.userRepository.findById(id);
+
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        var user = userOptional.get();
+
+        var existingUser = this.userRepository.findByUsername(userModel.getUsername());
+        if (existingUser != null && !existingUser.getId().equals(id)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já está em uso por outro usuário");
+        }
+
+        user.setName(userModel.getName());
+        user.setUsername(userModel.getUsername());
+
+        if (userModel.getPassword() != null && !userModel.getPassword().isBlank()) {
+            var passwordHashed = BCrypt.withDefaults()
+                    .hashToString(12, userModel.getPassword().toCharArray());
+            user.setPassword(passwordHashed);
+        }
+
+        var updatedUser = this.userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable UUID id) {
+        var userOptional = this.userRepository.findById(id);
+
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        this.userRepository.deleteById(id);
+        return ResponseEntity.ok("Usuário deletado com sucesso");
+
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserModel>> listAll() {
+        var users = this.userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
 }
